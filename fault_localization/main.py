df9e47158a31d06f238db8cb32d8cfcf55a30ba5
WojciechMojsiejuk <wojciechmojsiejuk@gmail.com>
2022-11-09 13:15:44 +0900

    Improve parser to read bad CSV faulty files

--- fault_localization/main.py
+++ fault_localization/main.py
@@ -8,12 +8,53 @@ from tqdm import tqdm
 import numpy as np
 from faults_extractor import faults_extractor
 
+
 class NotFoundInTheDataFrame(Exception):
     pass
 
+
+class FaultLocalizator:
+
+    def __init__(self, coverage_filepath, faults_filepath):
+        try:
+            self.coverage = pd.read_csv(coverage_filepath, skiprows=2, engine='python', sep=',')  # load coverage file, skip first two rows
+            self.faults = faults_extractor(faults_filepath)
+            self.number_of_faults_covered = np.sum(self.coverage['Line'].isin(self.faults['Line']))
+            if self.number_of_faults_covered == 0:
+                raise NotFoundInTheDataFrame()
+            self.FAILED_STATUS = False
+            self.formulas = []
+        except ParserError as pe:
+            warnings.warn(f'Fault with file {coverage_paths[idx]}')
+            warnings.warn(pe)
+            self.FAILED_STATUS = True
+        except NotFoundInTheDataFrame:
+            warnings.warn(f'Fault with file {coverage_paths[idx]}')
+            warnings.warn('Line from faulty csv was not found in the whole dataframe')
+            self.FAILED_STATUS = True
+
+    def calc_jaccard(self):
+        self.coverage['Jaccard'] = self.coverage['Ef'] / (self.coverage['Ef'] + self.coverage['Nf'] + self.coverage['Np'])  # calculate Jaccard similarity coefficient
+        self.formulas.append('Jaccard')
+
+    def calc_dstar(self, d=2):
+        dstar_name = 'DStart'+str(d)
+        self.coverage[dstar_name] = self.coverage['Ef'] ** d / (self.coverage['Ep'] + self.coverage['Nf'])
+        self.formulas.append(dstar_name)
+
+    def calc_rank(self, formula):
+        self.coverage['Rank'] = rankdata(self.coverage[formula], method='min')  # rank data based on coefficient
+        self.coverage.sort_values('Rank', ascending=False)  # sort data by rank
+
+    def calc_top(self, top):
+        top_values = self.coverage['Rank'].unique()[:top] # find top N unique values in rank
+        self.coverage_top = self.coverage.loc[self.coverage['Rank'].isin(top_values)]  # select top N unique values in dataframe
+
+    def check_within_top(self):
+        return np.sum(self.coverage_top['Line'].isin(self.faults['Line'])) == self.number_of_faults_covered  # if we found all faults
+
 data_path = './eval-data/eval/'
 TOP = 5
-
 if __name__ == '__main__':
     coverage_paths = glob.glob(data_path+'*.coverage.csv')  # find all coverage files
     faulty_paths = glob.glob(data_path+'*.faulty-lines.csv')  # find all faulty-lines files
@@ -24,41 +65,16 @@ if __name__ == '__main__':
     failed = 0
     for idx in tqdm(range(data_size)):
 
-        try:
-            df = pd.read_csv(coverage_paths[idx], skiprows=2, engine='python', sep=',')  # load coverage file, skip first two rows
-        except ParserError as pe:
-            warnings.warn(f'Fault with file {coverage_paths[idx]}')
-            warnings.warn(pe)
+        fault = FaultLocalizator(coverage_paths[idx], faulty_paths[idx])
+        if not fault.FAILED_STATUS:
+            fault.calc_jaccard()
+            fault.calc_rank(fault.formulas[0])
+            fault.calc_top(TOP)
+            if fault.check_within_top():
+                count += 1
+        else:
             failed += 1
-            count_all -= 1  # we must decrease a number of all element as this one cannot be used for evaluation
-            continue
-
-        df['Jaccard'] = df['Ef'] / (df['Ef'] + df['Nf'] + df['Np'])  # calculate Jaccard similarity coefficient
-        df['DStar2'] = df['Ef']**2 / (df['Ep'] + df['Nf'])
-        df['Rank'] = rankdata(df['Jaccard'], method='min')  # rank data based on coefficient
-        df = df.sort_values('Rank', ascending=False)  # sort data by rank
-        top = df['Rank'].unique()[:TOP]  # find top N unique values in rank
-        df_top = df.loc[df['Rank'].isin(top)]  # select top N unique values in dataframe
-    
-        try:
-            df_2 = faults_extractor(faulty_paths[idx])
-            if not df['Line'].isin(df_2['Line']).any():
-                raise NotFoundInTheDataFrame()
-        except ParserError as pe:
-            warnings.warn(f'Fault with file {faulty_paths[idx]}')
-            warnings.warn(str(pe))
-            failed += 1
-            count_all -= 1  # we must decrease a number of all element as this one cannot be used for evaluation
-            continue
-        except NotFoundInTheDataFrame:
-            warnings.warn(f'Fault with file {coverage_paths[idx]}')
-            warnings.warn('Line from faulty csv was not found in the whole dataframe')
-            failed += 1
-            count_all -= 1  # we must decrease a number of all element as this one cannot be used for evaluation
-            continue
-
-        if np.sum(df_top['Line'].isin(df_2['Line'])) == df_2.shape[0]: #if we found all faults
-            count += 1
+            count_all -= 1
 
     acc = count/count_all
     print(f'Accuracy to find faulty line within top {TOP} = {acc} with {failed} out of {data_size} failed files')
